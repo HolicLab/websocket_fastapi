1d3ffb314ffecfa330fbe1d6107060542a035204
root <lka1116@naver.com>
2025-04-23 10:37:17 +0000

    modify main

--- main.py
+++ main.py
@@ -53,4 +53,4 @@ async def websocket_endpoint(websocket: WebSocket):
         await task.cleanup()
         
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port="18001", reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=18001, reload=True)
